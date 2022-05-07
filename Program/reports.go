ea980d79b64b7cd11597b0543c92296c632d47fd
AlexandroProvenzale <3056350180301@ingenieria.usac.edu.gt>
2022-05-06 20:08:17 -0600

    Reporte file

--- Program/reports.go
+++ Program/reports.go
@@ -31,6 +31,194 @@ func Reportar(rep InfoRep) {
 
 	if strings.ToLower(rep.Name) == "tree" {
 		ReporteTree(path, partName, rep.Path)
+	} else if strings.ToLower(rep.Name) == "file" {
+		if rep.Ruta == "" {
+			fmt.Println("Error: Parámetro obligatorio Ruta vacío, es necesario para este reporte")
+			return
+		}
+		ReporteFile(path, partName, rep.Ruta)
+	}
+}
+
+func ReporteFile(path, partName, ruta string) {
+	file, err := os.OpenFile(path, os.O_RDWR, 0777) // Abrimos el archivo
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer func(file *os.File) { // Posponemos su cierre hasta que el método termine su ejecución
+		err := file.Close()
+		if err != nil {
+			fmt.Println("Error: Problema al cerrar el disco")
+			return
+		}
+	}(file)
+
+	if _, err := file.Seek(0, 0); err != nil { // Situamos el puntero en el principio
+		log.Fatal(err)
+		return
+	}
+	var mbr MBR
+	mbrbytes := make([]byte, unsafe.Sizeof(mbr))
+	if _, err := file.Read(mbrbytes); err != nil {
+		log.Fatal(err)
+		return
+	}
+	buff := bytes.NewBuffer(mbrbytes)
+	dec := gob.NewDecoder(buff)
+	if err := dec.Decode(&mbr); err != nil { // Obtenemos la información del mbr para buscar la partición
+		log.Fatal(err)
+		return
+	}
+	fmt.Println("-----------------------REPORTE FILE-----------------------")
+	var partition Partition
+	for j := 0; j < 4; j++ { // Buscamos la partición con el nombre almacenado que respecta a la del ID ingresado
+		partition = mbr.Partition[j]
+		if string(partition.Status) == "1" && string(partition.Name) == partName {
+			break
+		}
+	}
+	if reflect.DeepEqual(partition, Partition{}) {
+		fmt.Println("No se encontró la partición", partName, "en el disco", path)
+		return
+	}
+
+	if _, err := file.Seek(int64(BytesToInt(partition.Start)), 0); err != nil { // Situamos el puntero en el inicio de la particion
+		log.Fatal(err)
+		return
+	}
+	var sb SuperBloque
+	sbBytes := make([]byte, unsafe.Sizeof(sb)+50)
+	if _, err := file.Read(sbBytes); err != nil {
+		log.Fatal(err)
+		return
+	}
+	buff = bytes.NewBuffer(sbBytes)
+	dec = gob.NewDecoder(buff)
+	if err := dec.Decode(&sb); err != nil { // Obtenemos la información del superbloque
+		log.Fatal(err)
+		return
+	}
+	inodeActual := 0
+	blockActual := 0
+	pathCounter := 0
+	initInode := BytesToInt(sb.InodeStart)
+	rutas := strings.Split(ruta, "/")
+	busquedaFile(&sb, rutas, initInode, &inodeActual, &blockActual, &pathCounter, BytesToInt(partition.Start), file, false, ruta)
+}
+
+func busquedaFile(sb *SuperBloque, rutas []string, initInode int, inodeActual, blockActual, pathCounter *int, sbStart int, file *os.File, existeRuta bool, fullruta string) {
+	if _, err := file.Seek(int64(initInode), 0); err != nil {
+		log.Fatal(err)
+		return
+	}
+	var inodoActual Inodo
+	inodeBytes := make([]byte, BytesToInt(sb.InodeSize))
+	if _, err := file.Read(inodeBytes); err != nil {
+		log.Fatal(err)
+		return
+	}
+	buff := bytes.NewBuffer(inodeBytes)
+	dec := gob.NewDecoder(buff)
+	if err := dec.Decode(&inodoActual); err != nil {
+		log.Fatal(err)
+		return
+	}
+	block := BytesToIntArray(inodoActual.Block)
+	for i := range block {
+		if block[i] == -1 {
+			// something
+			break
+		}
+		*blockActual = block[i]
+		if *blockActual == 0 && rutas[*pathCounter] == "" {
+			*pathCounter += 1
+		}
+		initBlo := BytesToInt(sb.BlockStart) + BytesToInt(sb.BlockSize)**blockActual
+		if _, err := file.Seek(int64(initBlo), 0); err != nil {
+			log.Fatal(err)
+			return
+		}
+		if BytesToInt(inodoActual.Type) == 0 {
+			var bloqueCarpeta BloqueCarpeta
+			bloCarpBytes := make([]byte, BytesToInt(sb.BlockSize))
+			if _, err := file.Read(bloCarpBytes); err != nil {
+				log.Fatal(err)
+				return
+			}
+			buff = bytes.NewBuffer(bloCarpBytes)
+			dec = gob.NewDecoder(buff)
+			if err := dec.Decode(&bloqueCarpeta); err != nil {
+				log.Fatal(err)
+				return
+			}
+
+			for j := range bloqueCarpeta.BContent {
+				if string(bloqueCarpeta.BContent[j].Inodo) == "-1" {
+					// something
+					break
+				} else if string(bloqueCarpeta.BContent[j].Name) != "." && string(bloqueCarpeta.BContent[j].Name) != ".." {
+					//inodeActual
+					if string(bloqueCarpeta.BContent[j].Name) == rutas[*pathCounter] {
+						if rutas[*pathCounter] == rutas[len(rutas)-1] {
+							var contenidoArchivo string
+							inodoArchivo := BytesToInt(sb.InodeStart) + BytesToInt(sb.InodeSize)*BytesToInt(bloqueCarpeta.BContent[j].Inodo)
+							if _, err := file.Seek(int64(inodoArchivo), 0); err != nil {
+								log.Fatal(err)
+								return
+							}
+							var inArch Inodo
+							inArchBytes := make([]byte, BytesToInt(sb.InodeSize))
+							if _, err := file.Read(inArchBytes); err != nil {
+								log.Fatal(err)
+								return
+							}
+							buff = bytes.NewBuffer(inArchBytes)
+							dec = gob.NewDecoder(buff)
+							if err := dec.Decode(&inArch); err != nil {
+								log.Fatal(err)
+								return
+							}
+							bloqueArchivos := BytesToIntArray(inArch.Block)
+							for x := range bloqueArchivos {
+								if bloqueArchivos[x] == -1 {
+									break
+								}
+								initBloArchivo := BytesToInt(sb.BlockStart) + BytesToInt(sb.BlockSize)*bloqueArchivos[x]
+								if _, err := file.Seek(int64(initBloArchivo), 0); err != nil {
+									log.Fatal(err)
+									return
+								}
+								var bloqueArchivo BloqueArchivo
+								bloArchBytes := make([]byte, BytesToInt(sb.BlockSize))
+								if _, err := file.Read(bloArchBytes); err != nil {
+									log.Fatal(err)
+									return
+								}
+								buff = bytes.NewBuffer(bloArchBytes)
+								dec = gob.NewDecoder(buff)
+								if err := dec.Decode(&bloqueArchivo); err != nil {
+									log.Fatal(err)
+									return
+								}
+								contenidoArchivo += string(bloqueArchivo.Content)
+							}
+							fmt.Println("Archivo:", rutas[*pathCounter])
+							fmt.Println("Ruta completa:", fullruta)
+							fmt.Println("Contiene lo siguiente:")
+							fmt.Println("----------------------------------------------------------")
+							fmt.Println(contenidoArchivo)
+						}
+						*pathCounter += 1
+						// Hacer algo cuando encuentra la carpeta dentro de un bloque de carpetas
+						*inodeActual = BytesToInt(bloqueCarpeta.BContent[j].Inodo)
+						initIn := BytesToInt(sb.InodeStart) + BytesToInt(sb.InodeSize)**inodeActual
+						busquedaFile(sb, rutas, initIn, inodeActual, blockActual, pathCounter, sbStart, file, existeRuta, fullruta)
+						return
+					}
+				}
+			}
+		}
 	}
 }
 
